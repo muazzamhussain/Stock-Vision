28134b12b96b573966694a6a3b6a515719068cb6
Muazzam Hussain <muazzamsaeed09@gmail.com>
2026-07-01 21:46:50 +0500

    Project Setup

---
+++ tailwind.config.ts
@@ -0,0 +1,24 @@
+import type { Config } from "tailwindcss";
+
+const config: Config = {
+    darkMode: ["class"],
+    content: [
+        './pages/**/*.{ts,tsx}',
+        './components/**/*.{ts,tsx}',
+        './app/**/*.{ts,tsx}',
+        './src/**/*.{ts,tsx}',
+    ],
+    theme: {
+        extend: {
+            colors: {},
+            borderRadius: {
+                lg: 'var(--radius)',
+                md: 'calc(var(--radius) - 2px)',
+                sm: 'calc(var(--radius) - 4px)',
+            },
+        },
+    },
+    plugins: [],
+};
+
+export default config;
